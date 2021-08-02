1e92b1d9eb03526de88175c753d10314d8e0d44c
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-08-02 21:23:49 +0000

    added size option display in shoppping bag

--- bag/context.py
+++ bag/context.py
@@ -11,26 +11,40 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        product_total = quantity * product.price
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-            'product_total': product_total,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            product_total = item_data * product.price
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+                'product_total': product_total,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['items_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                product_total = quantity * product.price
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': quantity,
+                    'product': product,
+                    'size': size,
+                    'product_total': product_total,
+                })
 
     if total < settings.FREE_DELIVERY_TRESHOLD:
-        delivery = total*Decimal(settings.STANDARD_DELIVERY_PERCENTAGE/100)
-        free_delivery_delta = settings.FREE_DELIVERY_TRESHOLD - total
+        delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+        free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
     else:
         delivery = 0
         free_delivery_delta = 0
 
-    grand_total = total + delivery
+    grand_total = delivery + total
 
     context = {
         'bag_items': bag_items,
