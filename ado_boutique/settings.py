c7e4a11283a20454269325e81f05327c735ada3e
IgnazioSotgiu <ignazio_sotgiu@yahoo.it>
2021-08-15 00:29:42 +0000

    Add basic checkout functionality

--- ado_boutique/settings.py
+++ ado_boutique/settings.py
@@ -171,7 +171,7 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-#stripe
+# stripe
 
 FREE_DELIVERY_TRESHOLD = 50
 STANDARD_DELIVERY_PERCENTAGE = 10
